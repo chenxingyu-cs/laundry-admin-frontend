679337c40b804b9abd7bcb24c41afe1d2655cd29
Xingyu Chen <chenxingyu.cs@gmail.com>
2017-06-10 15:32:12 -0700

    build the fundation

---
+++ .roadhogrc.js
@@ -0,0 +1,42 @@
+// npm i antd-mobile -S
+const path = require('path');
+const pxtorem = require('postcss-pxtorem');
+const svgSpriteDirs = [
+  require.resolve('antd-mobile').replace(/warn\.js$/, ''), // antd-mobile 内置svg
+];
+
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [
+    'transform-runtime',
+    ['import', { libraryName: 'antd-mobile', style: true }],
+  ],
+  env: {
+    development: {
+      extraBabelPlugins: [
+        'dva-hmr',
+        'transform-runtime',
+      ],
+    },
+    production: {
+      extraBabelPlugins: [
+        'transform-runtime',
+      ],
+    },
+  },
+  svgSpriteLoaderDirs: svgSpriteDirs,
+  extraPostCSSPlugins: [
+    pxtorem({
+      rootValue: 100,
+      propWhiteList: [],
+    }),
+  ],
+  theme: {
+    "@brand-primary": "#f45675",
+    "@primary-color": "#f45675",
+    "@link-color": "#f45675",
+    "@border-radius-base": "2px",
+    "@font-size-base": "50px",
+    "@line-height-base": "1.2"
+  }
+}
---
+++ src/components/MainLayout/Header.js
@@ -0,0 +1,16 @@
+import { NavBar, Icon } from 'antd-mobile';
+import React from 'react';
+
+function Header() {
+  return (
+    <NavBar
+      leftContent="back" mode="light" onLeftClick={() => console.log('onLeftClick')}
+      rightContent={[
+        <Icon key="0" type="search" style={{ marginRight: '0.32rem' }} />,
+        <Icon key="1" type="ellipsis" />,
+      ]}
+    >导航</NavBar>
+  );
+}
+
+export default Header;
