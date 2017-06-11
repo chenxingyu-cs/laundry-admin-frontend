887518c4ff99eca6201ee10ebf22890ae6f9e2a0
Xingyu Chen <chenxingyu.cs@gmail.com>
2017-06-11 14:03:48 -0700

    modify the routers

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Router, Route, Redirect } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import AdminApp from './routes/AdminApp.js';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Redirect from="/" to="/admin" />
-      <Route path="/admin" component={IndexPage} />
+      <Redirect from="/" to="/admin/menu" />
+      <Route path="/admin" component={AdminApp}>
+        <Route path="menu" component={IndexPage} />
+      </Route>
+
     </Router>
   );
 }
