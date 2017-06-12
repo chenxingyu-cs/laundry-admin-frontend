885e357f9fade619a7082ba86665014d52f3fb19
Xingyu Chen <chenxingyu.cs@gmail.com>
2017-06-12 01:53:03 -0700

    add link from menu

--- src/components/IndexPage/IndexModuleItem.js
+++ src/components/IndexPage/IndexModuleItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { browserHistory } from 'dva/router';
 import styles from './IndexModuleItem.css';
 
 function IndexModuleItem({ item }) {
   return (
-    <div className={styles.normal}>
+    <div className={styles.normal} onClick={() => browserHistory.push(item.url)}>
       <img src={item.itemImg} role="presentation" />
       <div>{item.itemTitle}</div>
     </div>
