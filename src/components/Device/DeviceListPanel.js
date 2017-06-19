fb325af7ece9c893a340cc1f1f7189b8567e841a
arbiter527 <arbiter527@gmail.com>
2017-06-18 19:37:41 -0700

    Fix lint

--- src/components/Device/DeviceListPanel.js
+++ src/components/Device/DeviceListPanel.js
@@ -1,12 +1,11 @@
 import { connect } from 'dva';
-import { List, Tabs, WhiteSpace } from 'antd-mobile';
+import { List, Tabs } from 'antd-mobile';
 import React from 'react';
 import styles from './DeviceListPanel.css';
 
 const TabPane = Tabs.TabPane;
 
 function DeviceListPanel({ free, ongoing }) {
-
   function callback(key) {
     console.log('onChange', key);
     console.log(free);
@@ -19,9 +18,9 @@ function DeviceListPanel({ free, ongoing }) {
 
   function jumpToOrderDetail(boxId, status) {
     if (status === '空闲') {
-      alert('跳转启用');
+      alert('跳转启用'); // eslint-disable-line
     } else if (status === '使用中') {
-      alert('跳转编辑');
+      alert('跳转编辑'); // eslint-disable-line
     }
   }
 
@@ -30,18 +29,26 @@ function DeviceListPanel({ free, ongoing }) {
       <TabPane tab="未启用设备" key="1">
         <List>
           {free.map(device =>
-            <div key={device.boxId} className={styles.card} onClick={e => jumpToOrderDetail(device.boxId, device.status)}>
+            <div
+              key={device.boxId}
+              className={styles.card}
+              onClick={() => jumpToOrderDetail(device.boxId, device.status)}
+            >
               工控机ID {device.boxId}
-            </div>
+            </div>,
           )}
         </List>
       </TabPane>
       <TabPane tab="已启用设备" key="2">
         <List>
           {ongoing.map(device =>
-            <div key={device.boxId} className={styles.card} onClick={e => jumpToOrderDetail(device.boxId, device.status)}>
+            <div
+              key={device.boxId}
+              className={styles.card}
+              onClick={() => jumpToOrderDetail(device.boxId, device.status)}
+            >
               工控机ID {device.boxId}
-            </div>
+            </div>,
           )}
         </List>
       </TabPane>
--- src/components/Device/DeviceSearchPanel.js
+++ src/components/Device/DeviceSearchPanel.js
@@ -32,13 +32,14 @@ class DeviceSearchPanel extends React.Component {
   }
 
   scan = () => {
-    alert("扫一扫");
-    wx.scanQRCode({
-      needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-      scanType:["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-      success: function (res) {
-        var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-      }
+    alert('扫一扫'); // eslint-disable-line
+    wx.scanQRCode({ // eslint-disable-line
+      needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果
+      // 可以指定扫二维码还是一维码，默认二者都有
+      scanType:['qrCode', 'barCode'], // eslint-disable-line
+      success: function (res) { // eslint-disable-line
+        let result = res.resultStr; // eslint-disable-line
+      },
     });
   }
 
@@ -58,6 +59,7 @@ class DeviceSearchPanel extends React.Component {
         <div className={styles.badge}>
           <img
             src="http://widgetsmartloc.esy.es/icons/qr-scanner.png"
+            alt="Scan QR Code"
             onClick={this.scan}
           />
         </div>
